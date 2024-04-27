c43668598ba2c0e9a0d63c59215b9bcc84943c56
Ahmed AbuElkheir <ahmedhaboelkheir@gmail.com>
2024-04-27 21:45:42 +0300

    Now you can go from a product to the other , coursel left on main page and other modifications to prefect it

---
+++ WebApplication/Ultimus/Ultimus/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ultimus.Models;
+
+namespace Ultimus.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly UltimusEntities _context;
+
+        public ItemsController()
+        {
+            _context = new UltimusEntities(); // Initialize your DbContext
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Product product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("items", product); // Specify the view name "items" explicitly
+        }
+    }
+}
