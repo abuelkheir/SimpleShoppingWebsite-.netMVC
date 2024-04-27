622a19459d0843fc2acf8fc3a34344de7cc2af40
Ahmed AbuElkheir <ahmedhaboelkheir@gmail.com>
2024-04-27 14:07:05 +0300

    had few bumps but made me grasp things a lot better

---
+++ WebApplication/Ultimus/Ultimus/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ultimus.Models;
+
+
+namespace Ultimus.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        private UltimusEntities db = new UltimusEntities();
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category);
+            return View(products.ToList());
+         
+        }
+    }
+}
---
+++ WebApplication/Ultimus/Ultimus/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ultimus.MvcApplication" Language="C#" %>
