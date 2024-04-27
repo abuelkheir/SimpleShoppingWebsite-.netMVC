50e9d7d0080212281125ef2bc324e5d1dd77fd29
Ahmed AbuElkheir <ahmedhaboelkheir@gmail.com>
2024-04-27 11:16:37 +0300

    Added First page to project , Using Entity framewor

---
+++ WebApplication/UltimusApp/UltimusApp/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UltimusApp.Models
+{
+    public class Product : Category
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductRating { get; set; }
+    }
+}
